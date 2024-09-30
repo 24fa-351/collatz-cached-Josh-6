ced28caba02fa87a189272ae2bac9fe9114feffe
Josh-6 <Joshuabarpna@gmail.com>
2024-09-29 19:58:51 -0700

    Trying to do memo

--- collatz.c
+++ collatz.c
@@ -2,25 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
-//#include "map.h"
+#include "map.h"
 #include "collatz_calc.h"
 
 int64_t collat(int64_t number)
 {
     if (number % 2 == 0)
     {
-        return number/2;
+        return number / 2;
     }
     else
-    {   
+    {
         return (3 * number) + 1;
     }
 }
 
 int64_t step_counter(int num)
 {
-    int steps = 0;
-
     while (num != 1)
     {
         num = collatz(num);
@@ -30,33 +28,37 @@ int64_t step_counter(int num)
     return steps;
 }
 
+
 int main(int argc, char *argv[])
 {
     int num_of_rand = atoi(argv[1]);
     int min = atoi(argv[2]);
     int max = atoi(argv[3]);
 
-    int steps = 0;
+
     int total = 0;
-
-    for(int i = 0; i < num_of_rand; i++)
+    if (argc == 4)
     {
-        int random_num = rand() % (max - min + 1) + min;
-        steps = step_counter(random_num);
-        total += steps;
-        //printf("Number of steps for %d: %d\n", random_num, steps);
+        for (int i = 0; i < num_of_rand; i++)
+        {
+            int random_num = rand() % (max - min + 1) + min;
+            steps = step_counter(random_num);
+            total += steps;
+            //printf("Number of steps: %d\n", steps);
+        }
+        int avg_steps = total / num_of_rand;
+        printf("Average steps: %d\n", avg_steps);
+        printf("Size of char: %d\n", argc);
     }
-    int avg_steps = total/num_of_rand;
-    printf("Average steps: %d\n", avg_steps);
-
-    //with cache
-    /*
-    for(int i = 0; i < num_of_rand; i++)
+    else if(argc == 6)
     {
-        int random_num = rand() % (max - min + 1) + min;
-        memo = fib_provider;
-        //steps = fib_provider(random_num);
+        // with cache 
+        steps = memoize(num_of_rand, max, min, argv[4], atoi(argv[5]));
+        
     }
-    */
+    else
+    {
+        printf("To many arguments");
+    }
     return 0;
 }
--- map.h
+++ map.h
@@ -1,96 +1,115 @@
 #ifndef map
 #define map
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
-#include <inttypes.h> 
+#include <inttypes.h>
 #include "collatz_calc.h"
 
-#define MAX_SIZE 1000 // Maximum number of elements in the map 
-  
-int size = 0; // Current number of elements in the map 
+int MAX_SIZE;
+
+int size = 0; // Current number of elements in the map
 
 typedef int64_t (*Functionpointer)(int);
 Functionpointer fib_provider = NULL;
 
+int getIndex(int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (memo_num[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1; // Key not found
+}
+
+// Function to insert a key-value pair into the map
+void insert(int key, int value)
+{
+    int index = getIndex(key);
+    if (index == -1)
+    { // Key not found
+        memo_num[size] = key;
+        memo_steps[size] = value;
+        size++;
+    }
+    else
+    { // Key found
+        memo_steps[index] = value;
+    }
+}
+
+// Function to get the value of a key in the map
+int get(int key)
+{
+    int index = getIndex(key);
+    if (index == -1)
+    { // Key not found
+        return -1;
+    }
+    else
+    { // Key found
+        return memo_steps[index];
+    }
+}
+
+// Function to print the map
+void printMap()
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%ld: %ld\n", memo_num[i], memo_steps[i]);
+    }
+}
+
 int64_t *memo_num = NULL;
 int64_t *memo_steps = NULL;
-
-int getIndex(int key) 
-{ 
-    for (int i = 0; i < size; i++) { 
-        if (memo_num[i] == key) 
-        { 
-            return i; 
-        } 
-    } 
-    return -1; // Key not found 
-} 
-  
-// Function to insert a key-value pair into the map 
-void insert(int key, int value) 
-{ 
-    int index = getIndex(key); 
-    if (index == -1) { // Key not found 
-        memo_num[size] = key; 
-        memo_steps[size] = value; 
-        size++; 
-    } 
-    else { // Key found 
-        memo_steps[index] = value; 
-    } 
-} 
-  
-// Function to get the value of a key in the map 
-int get(int key) 
-{ 
-    int index = getIndex(key); 
-    if (index == -1) { // Key not found 
-        return -1; 
-    } 
-    else { // Key found 
-        return memo_steps[index]; 
-    } 
-} 
-  
-// Function to print the map 
-void printMap() 
-{ 
-    for (int i = 0; i < size; i++) { 
-        printf("%ld: %ld\n", memo_num[i], memo_steps[i]); 
-    } 
-}
-
-int64_t fib_memo(int user_num)
+int steps = 0;
+int hits = 0;
+int miss = 0;
+int64_t memo(int num)
 {
-   if (memo_num == NULL)
-   {
-      memo_num = (int64_t *)malloc((MAX_SIZE) * sizeof(int64_t));
-      memo_steps = (int64_t *)malloc((MAX_SIZE) * sizeof(int64_t));
-      
-      for (int i = 0; i < 1000; i++)
-      {
-         memo_num[i] = -1;
-      }
-   }
+    if (memo_num == NULL)
+    {
+        memo_num = (int64_t *)malloc((MAX_SIZE) * sizeof(int64_t));
+        memo_steps = (int64_t *)malloc((MAX_SIZE) * sizeof(int64_t));
 
-   if (memo_num[user_num] == -1)
-   {
-      memo_num[user_num] = collatz(user_num);
-      //printf("Adding to cache: %d\n", user_num);
-   }
-   /*else
-   {
-      printf("Recieving from cache: %d\n", user_num);
-   }
-    */
-   return memo_num[user_num];
+        for (int i = 0; i < MAX_SIZE; i++)
+        {
+            memo_num[i] = -1;
+        }
+    }
+    int code = 1;
+    if(num != 1)
+    {
+        num = collat(num);
+        steps++;
+    }
+    if (memo_num[size] == -1)
+    {
+        memo_num[size] = collatz(num);
+        // printf("Adding to cache: %d\n", user_num);
+    }
+    /*else
+     {
+         printf("Recieving from cache: %d\n", user_num);
+     }
+     */
+    return memo_num[num];
 }
 
-int memo(int random, char policy[])
+int memoize(int N, int max, int min, char *policy[], int cache_size)
 {
-    for (int i = 0; i < 1000; i++)
-      {
-         memo_num[i] = -1;
-      }
+    MAX_SIZE = cache_size;
+    int step;
+    fib_provider = memo;
+    for (int i = 0; i < N; i++)
+    {
+        int random_num = rand() % (max - min + 1) + min;
+        steps = memo(random_num);
+    }
+
+    return step;
 }
-#endif //map
+#endif // map
